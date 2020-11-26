8d747ebec83108991b8596c47ce9e6b9158f33d7
misato-yabe <mumumutt58@gmail.com>
2020-11-26 19:36:35 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,12 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
 
   def create
-
     @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
