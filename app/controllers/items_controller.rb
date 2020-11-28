74f82f2c7ef50cba38cfc06e46e8f8af3cdabfc6
misato-yabe <mumumutt58@gmail.com>
2020-11-28 15:33:45 +0900

    商品削除機能　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only:[:edit,:new]
+  before_action :authenticate_user!,only:[:edit,:new,:destroy]
   before_action :set_item,only:[:show,:edit,:update,:destroy]
   before_action :move_to_index, only: [:edit, :destroy]
 
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
